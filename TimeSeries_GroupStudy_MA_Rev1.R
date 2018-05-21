d26d0cd1feffa42974e2d6fdf887784cef8bb498
mohandas98 <34007474+mohandas98@users.noreply.github.com>
2018-05-21 19:16:52 +0530

    Add files via upload

--- TimeSeries_GroupStudy_MA_Rev1.R
+++ TimeSeries_GroupStudy_MA_Rev1.R
@@ -1,7 +1,7 @@
 #install.packages("graphics")
 
-#library(dplyr)
-#library(graphics)
+library(dplyr)
+library(graphics)
 
 #Load the metadata
 metadata<- read.csv("Global Superstore.csv", header = T, sep = ',',stringsAsFactors=FALSE)
@@ -86,7 +86,7 @@ for(i in 1:n)
     #Find the coefficient of variation
     m <- mean(tempSum$Profit)
     sigma <- sd(tempSum$Profit)
-    cv<-m/sigma
+    cv<-sigma/m
 
     #Store the result for comparison later
     Result[i,1]<-as.character(grid$.market[i])
@@ -104,27 +104,27 @@ Result<-Result[order(Result$cv,decreasing = TRUE),]
 Result
 
 #      Mrkt        Sgmt MeanProfit  SdProfit        cv
-# 5      EU    Consumer  3930.9939 2454.1398 1.6017807
-# 2    APAC    Consumer  4642.0325 2934.3785 1.5819474
-# 6   LATAM    Consumer  2513.1861 1662.4295 1.5117550
-# 9    APAC   Corporate  2702.8591 1886.8305 1.4324864
-# 12     EU   Corporate  2570.7079 1963.5252 1.3092309
-# 13  LATAM   Corporate  1205.7379  978.0003 1.2328605
-# 14     US   Corporate  1916.2320 1920.8479 0.9975969
-# 7      US    Consumer  2794.1502 2828.7698 0.9877616
-# 16   APAC Home Office  1738.4428 1818.3736 0.9560427
-# 21     US Home Office  1256.2225 1377.0048 0.9122862
-# 19     EU Home Office  1265.5845 1413.0343 0.8956502
-# 20  LATAM Home Office   898.6486 1056.5392 0.8505587
-# 1  Africa    Consumer   995.2521 1313.3196 0.7578140
-# 3  Canada    Consumer   230.4214  321.5098 0.7166855
-# 10 Canada   Corporate   148.1312  230.0144 0.6440083
-# 8  Africa   Corporate   430.9784  765.4631 0.5630297
-# 15 Africa Home Office   425.2618  761.2168 0.5586606
-# 4    EMEA    Consumer   531.9286 1164.0039 0.4569818
-# 17 Canada Home Office   124.1292  278.4790 0.4457399
-# 11   EMEA   Corporate   260.3986 1163.2273 0.2238588
-# 18   EMEA Home Office   122.2138  718.7085 0.1700464
+# 18   EMEA Home Office   122.2138  718.7085 5.8807467
+# 11   EMEA   Corporate   260.3986 1163.2273 4.4671024
+# 17 Canada Home Office   124.1292  278.4790 2.2434607
+# 4    EMEA    Consumer   531.9286 1164.0039 2.1882709
+# 15 Africa Home Office   425.2618  761.2168 1.7899957
+# 8  Africa   Corporate   430.9784  765.4631 1.7761054
+# 10 Canada   Corporate   148.1312  230.0144 1.5527751
+# 3  Canada    Consumer   230.4214  321.5098 1.3953122
+# 1  Africa    Consumer   995.2521 1313.3196 1.3195849
+# 20  LATAM Home Office   898.6486 1056.5392 1.1756978
+# 19     EU Home Office  1265.5845 1413.0343 1.1165073
+# 21     US Home Office  1256.2225 1377.0048 1.0961473
+# 16   APAC Home Office  1738.4428 1818.3736 1.0459784
+# 7      US    Consumer  2794.1502 2828.7698 1.0123900
+# 14     US   Corporate  1916.2320 1920.8479 1.0024089
+# 13  LATAM   Corporate  1205.7379  978.0003 0.8111217
+# 12     EU   Corporate  2570.7079 1963.5252 0.7638072
+# 9    APAC   Corporate  2702.8591 1886.8305 0.6980869
+# 6   LATAM    Consumer  2513.1861 1662.4295 0.6614828
+# 2    APAC    Consumer  4642.0325 2934.3785 0.6321323
+# 5      EU    Consumer  3930.9939 2454.1398 0.6243052
 
 
-#Top two Market segments are EU-Consumer and APAC-Consumer
+#Top two Market segments are EMEA Home Office andEMEA   Corporate
